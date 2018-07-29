270185557184393ce2914a735ad6c95bbb083a76
BoutikKaneriya <kaneriya.bhoutik81@gmail.com>
2018-07-29 14:22:35 -0700

    Init Commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,17 @@
+const {MongoClient, ObjectID} = require('mongodb')
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp' , (err, client) => {
+    if(err) {
+        return console.log('Unable to connect to the mongodb server')
+    }
+    const db = client.db('TodoApp')
+
+db.collection('Users').find().count().then((docs)=> {
+    console.log(JSON.stringify(docs,undefined,2))
+}, (err) => {
+    if(err) {
+        console.log(err)
+    }
+})
+
+}) 
