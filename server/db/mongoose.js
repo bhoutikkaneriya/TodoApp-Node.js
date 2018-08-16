d9fb7c7df33a5cee65f8e096bf0134f562cca4b5
BoutikKaneriya <kaneriya.bhoutik81@gmail.com>
2018-08-16 02:35:29 -0700

    updated code for mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/TodoApp')
+mongoose.connect('mongodb://bhoutik:aeroplane1234@ds223542.mlab.com:23542/todoapp')
 
 module.exports = {
     mongoose
